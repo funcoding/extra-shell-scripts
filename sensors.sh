0ea3666731a45291d65f98bfbe158c9f9cf8ae57
vinay <vinaykumar1989@gmail.com>
2014-07-15 20:52:09 +0530

    Keep track of Core Temperature

--- sensors.sh
+++ sensors.sh
@@ -4,10 +4,6 @@ sensors | tee ~/Desktop/sensors.txt
 
 temp=`grep -oP 'Core 0: +\K[+-]\d+' ~/Desktop/sensors.txt`
 
-#temp=`grep -oP '^Core 0:\s+\K\+\d+[.]\d+?' ~/Desktop/sensors.txt`
-
-# echo '2-1.1'|sudo tee /sys/bus/usb/drivers/usb/bind
-
 #Calculation Part
 if [ $temp -gt +70 ]
  then 
